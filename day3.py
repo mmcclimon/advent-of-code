a0f5a426f005ccb3ff720ecebcb433a47db6f923
Michael McClimon <michael@mcclimon.org>
2017-12-03 18:11:25 -0500

    day3: be more pythonic

--- day3.py
+++ day3.py
@@ -1,62 +1,63 @@
 #!/usr/bin/env python
 
-class Spiral():
-
-    turns = { 'E': 'N', 'N': 'W', 'W': 'S', 'S': 'E' }
-
-    def __init__(self, limit, case='easy'):
+class Spiral(object):
+    def __init__(self):
         self.spiral = {}
         self.coordinates = {}
-        self.limit = limit
-        self.case = case
+        self.gen = self._coord_gen()
 
-        self._make_spiral()
+    def _coord_gen(self):
+        turns = { 'E': 'N', 'N': 'W', 'W': 'S', 'S': 'E' }
+        steps = {
+            'E': lambda x,y: (x+1, y),
+            'N': lambda x,y: (x, y+1),
+            'W': lambda x,y: (x-1, y),
+            'S': lambda x,y: (x, y-1),
+        }
 
-    def _make_spiral(self):
         x = 0
         y = 0
         facing = 'E'
         cur_len = 1
         steps_in_dir = 0
 
+        while True:
+            yield (x, y)
 
-        for n in xrange(1, self.limit + 1):
-            val = self._get_val_for_square(n, x, y)
-
-            self.spiral[n] = val
-            self.coordinates[(x,y)] = val
-
-            # Do the step
-            if facing == 'E': x += 1
-            if facing == 'N': y += 1
-            if facing == 'W': x -= 1
-            if facing == 'S': y -= 1
+            x, y = steps[facing](x, y)
             steps_in_dir += 1
 
             # We've now stepped: if we've gone enough, turn and reset the counter
             if steps_in_dir == cur_len:
-                facing = self.turns[facing]
+                facing = turns[facing]
                 steps_in_dir = 0
                 if facing == 'E' or facing == 'W':
                     cur_len += 1
 
 
 class EasySpiral(Spiral):
-    def __init__(self, limit):
-        Spiral.__init__(self, limit)
+    def __init__(self):
+        super(EasySpiral, self).__init__()
 
-    def _get_val_for_square(self, n, x, y):
-        return (x, y)
+    def _extend_spiral(self, limit):
+        for n in xrange(1, limit + 1):
+            if self.spiral.get(n, False):
+                continue
+
+            xy = self.gen.next()
+            self.spiral[n] = xy
 
     def get_taxicab_distance(self, n):
+        self._extend_spiral(n)
         x, y = self.spiral[n]
         return abs(x) + abs(y)
 
 class HardSpiral(Spiral):
-    def __init__(self, limit):
-        Spiral.__init__(self, limit)
+    def __init__(self):
+        Spiral.__init__(self)
 
-    def _get_val_for_square(self, n, x, y):
+    def _get_val_for_square(self, xy):
+        x, y = xy
         total = 0
 
         for xcur in [ x-1, x, x+1 ]:
@@ -67,18 +68,26 @@ class HardSpiral(Spiral):
         return total if total else 1
 
     def get_first_over(self, limit):
-        for i in xrange(1, self.limit + 1):
-            if self.spiral[i] > limit:
-                return self.spiral[i]
+        for n in xrange(1, limit + 1):
+            if self.spiral.get(n, False):
+                continue
 
+            xy = self.gen.next()
+            val = self._get_val_for_square(xy)
+            self.spiral[n] = val
+            self.coordinates[xy] = val
+
+            if val > limit:
+                return val
+
+       return None
 
 
 if __name__ == '__main__':
     n = 277678
-    easy = EasySpiral(n)
+    easy = EasySpiral()
+    hard = HardSpiral()
     print "Easy case for %s: %s steps" % (n, easy.get_taxicab_distance(n))
-
-    hard = HardSpiral(n)
-    print "Hard case for %s: %s is 1st over limit" % (n, hard.get_first_over(n))
+    # print "Hard case for %s: %s is 1st over limit" % (n, hard.get_first_over(n))
 
 
