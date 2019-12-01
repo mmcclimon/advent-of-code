aa0db76b43ffa08fb0e929c6a18686a00d621b64
Michael McClimon <michael@mcclimon.org>
2019-12-01 12:24:29 -0500

    Move some utils code into a module

---
+++ 2019/advent-utils.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const readline = require("readline");
+
+// synchronous, read whole files into memory and return arrays
+exports.fileLines = (fn) => fs.readFileSync(fn)
+                              .toString()
+                              .split("\n")
+                              .filter(s => s.length > 0);
+
+exports.fileLinesInt = (fn) => exports.fileLines(fn).map(n => parseInt(n));
+
+// async but require callbacks
+exports.getLines = async (fn, cb) => {
+    const rl = readline.createInterface({ input: fs.createReadStream(fn) });
+    for await (const line of rl) {
+        cb(line);
+    }
+};
+
+exports.getLinesInt = async (filename, cb) => {
+    return exports.getLines(filename, (line) => cb(parseInt(line)));
+};
--- 2019/day1.js
+++ 2019/day1.js
@@ -1,22 +1,7 @@
-const fs = require("fs");
-const readline = require("readline");
-
-const getLines = async function (filename, cb) {
-    const rl = readline.createInterface({
-        input: fs.createReadStream(filename),
-        crlfDelay: Infinity,
-    });
-
-    for await (const line of rl) {
-        cb(line);
-    }
-};
-
-const getLinesInt = async function (filename, cb) {
-    return getLines(filename, (line) => cb(parseInt(line)));
-}
+const utils = require('./advent-utils.js');
 
 const fuelRequired = (mass) => Math.floor(mass / 3) - 2;
+
 const fuelRequiredExtra = (mass) => {
     const fuel = fuelRequired(mass);
     return fuel >= 0 ? fuel + fuelRequired(fuel) : fuel;
@@ -25,7 +10,7 @@ const fuelRequiredExtra = (mass) => {
 let sum1 = 0;
 let sum2 = 0;
 
-getLinesInt('input-day1.txt', (mass) => {
+utils.getLinesInt('input-day1.txt', (mass) => {
     sum1 += fuelRequired(mass);
     sum2 += fuelRequiredExtra(mass);
 }).then(() => {
