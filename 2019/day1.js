27cca16956882e537326c3271c2216cb784d9052
Michael McClimon <michael@mcclimon.org>
2019-12-01 14:29:22 -0500

    day 1: a solution I'm happy with

--- 2019/day1.js
+++ 2019/day1.js
@@ -1,18 +1,18 @@
 const utils = require('./advent-utils.js');
 
-const fuelRequired = (mass) => Math.floor(mass / 3) - 2;
-
+const doSum = (arr, func) => arr.reduce((acc, el) => acc + func(el), 0)
+const fuelRequired = (mass) => Math.max(Math.floor(mass / 3) - 2, 0)
 const fuelRequiredExtra = (mass) => {
     const fuel = fuelRequired(mass);
-    return fuel >= 0 ? fuel + fuelRequired(fuel) : fuel;
+    return fuel === 0 ? fuel : fuel + fuelRequiredExtra(fuel);
 };
 
-let sum1 = 0;
-let sum2 = 0;
+// make sure it works
+const tests = [ 14, 1969, 100756 ];
+console.assert(doSum(tests, fuelRequired) === 34239, 'part 1 is wrong');
+console.assert(doSum(tests, fuelRequiredExtra) === 51314, 'part 2 is wrong');
 
-utils.getLinesInt('input-day1.txt', (mass) => {
-    sum1 += fuelRequired(mass);
-    sum2 += fuelRequiredExtra(mass);
-}).then(() => {
-    console.log(sum1, sum2);
-});
+// do the thing
+const lines = utils.fileLinesInt('input-day1.txt');
+console.log('part 1: ' + doSum(lines, fuelRequired));
+console.log('part 2: ' + doSum(lines, fuelRequiredExtra));
