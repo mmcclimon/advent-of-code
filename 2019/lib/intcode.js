6b0d43c139f168d8d12a02f568900e3f6dba990c
Michael McClimon <michael@mcclimon.org>
2019-12-05 12:54:10 -0500

    Refactor intcode.js a bit

--- 2019/lib/intcode.js
+++ 2019/lib/intcode.js
@@ -7,112 +7,99 @@ const MODES = {
 };
 
 const OpCode = class {
-  constructor (codeval, func) {
-    this.code = codeval;
+  constructor ({ code, func, resolve = [] }) {
+    this.code = code;
     this.func = func;
-    this.numArgs = func.length - 1; // account for modes as last one
-    this.compiled = this.compile();
+    this.numArgs = func.length;
+
+    // I don't really like this "should I resolve the thing" logic, but I'm
+    // done fiddling for now.
+    this._resolveIndices = new Set(resolve);
   };
 
-  // We want to encapsulate the data-slicing and arg-passing, so we'll munge
-  // this into a 0-ary function that wraps this.func and calls the it with the
-  // list of arguments it expects.
-  compile () {
-    const basefunc = this.func;
-    const offset = this.numArgs + 1;
+  run (cpu, funcargs) {
+    this.func.apply(cpu, funcargs);
+  }
 
-    return function (modes) {
-      const pointerBefore = this.pointer;
-
-      const funcArgs = this.data.slice(this.pointer + 1, this.pointer + offset);
-      funcArgs.push(modes);
-      basefunc.apply(this, funcArgs);
-
-      // do not bump pointer if instruction moved it.
-      if (this.pointer === pointerBefore) {
-        this.pointer += offset;
-      }
-    };
+  shouldResolveParam (i) {
+    return this._resolveIndices.has(i);
   }
 };
 
 const defaultOpcodes = [
   // addition
-  new OpCode(1, function (x, y, retpos, modes) {
-    const xval = this._resolveValue(x, modes[0]);
-    const yval = this._resolveValue(y, modes[1]);
-    this.data[retpos] = xval + yval;
+  new OpCode({
+    code: 1,
+    resolve: [0, 1],
+    func: function (x, y, retpos) { this.data[retpos] = x + y },
   }),
 
   // multiplication
-  new OpCode(2, function (x, y, retpos, modes) {
-    const xval = this._resolveValue(x, modes[0]);
-    const yval = this._resolveValue(y, modes[1]);
-    this.data[retpos] = xval * yval;
+  new OpCode({
+    code: 2,
+    resolve: [0, 1],
+    func: function (x, y, retpos) { this.data[retpos] = x * y },
   }),
 
   // input
-  new OpCode(3, function (pos, modes) {
-    this.data[pos] = this.inputs.shift();
+  new OpCode({
+    code: 3,
+    func: function (pos) { this.data[pos] = this.inputs.shift() },
   }),
 
   // output
-  new OpCode(4, function (pos, modes) {
-    this.outputs.push(this.data[pos]);
+  new OpCode({
+    code: 4,
+    func: function (pos) { this.outputs.push(this.data[pos]) },
   }),
 
   // jump-if-true
-  new OpCode(5, function (c, v, modes) {
-    const cond = this._resolveValue(c, modes[0]);
-    const val = this._resolveValue(v, modes[1]);
-    if (cond !== 0) {
-      this.pointer = val;
-    }
+  new OpCode({
+    code: 5,
+    resolve: [0, 1],
+    func: function (cond, val) { this.pointer = cond !== 0 ? val : this.pointer },
   }),
 
   // jump-if-false
-  new OpCode(6, function (c, v, modes) {
-    const cond = this._resolveValue(c, modes[0]);
-    const val = this._resolveValue(v, modes[1]);
-    if (cond === 0) {
-      this.pointer = val;
-    }
+  new OpCode({
+    code: 6,
+    resolve: [0, 1],
+    func: function (cond, val) { this.pointer = cond === 0 ? val : this.pointer },
   }),
 
   // less-than
-  new OpCode(7, function (x, y, pos, modes) {
-    const xval = this._resolveValue(x, modes[0]);
-    const yval = this._resolveValue(y, modes[1]);
-    this.data[pos] = xval < yval ? 1 : 0;
+  new OpCode({
+    code: 7,
+    resolve: [0, 1],
+    func: function (x, y, pos) { this.data[pos] = x < y ? 1 : 0 },
   }),
 
   // equals
-  new OpCode(8, function (x, y, pos, modes) {
-    const xval = this._resolveValue(x, modes[0]);
-    const yval = this._resolveValue(y, modes[1]);
-    this.data[pos] = xval === yval ? 1 : 0;
+  new OpCode({
+    code: 8,
+    resolve: [0, 1],
+    func: function (x, y, pos) { this.data[pos] = x === y ? 1 : 0 },
   }),
 
   // halt
-  new OpCode(99, function (modes) {
-    this.isRunning = false;
+  new OpCode({
+    code: 99,
+    func: function () { this.isRunning = false },
   }),
 ];
 
 exports.OpCode = OpCode;
 
 exports.IntCode = class {
-  constructor (memory, { opcodes, addDefaultOpcodes }) {
+  constructor (memory) {
     this.memory = memory;
     this.isRunning = false;
-    this.opcodes = opcodes !== undefined ? opcodes : new Map();
+    this.opcodes = new Map();
     this.pointer = 0;
     this.inputs = [];
     this.outputs = [];
 
-    if (addDefaultOpcodes) {
-      defaultOpcodes.forEach(op => this.addOpcode(op));
-    }
+    defaultOpcodes.forEach(op => this.addOpcode(op));
   }
 
   addOpcode (op, func, force = false) {
@@ -133,32 +120,40 @@ exports.IntCode = class {
 
     while (this.isRunning) {
       const instruction = this.data[this.pointer];
-      const [func, modes] = this._resolveInstruction(instruction);
-      func.call(this, modes);
+      this.runInstruction(instruction);
     }
 
     this.data = undefined;
     return this.outputs;
   }
 
-  // 1002 should compile to 2, [0, 1])
-  _resolveInstruction (instruction) {
+  runInstruction (instruction) {
     const s = String(instruction);
 
     const opcode = s.slice(-2);
     const op = this.opcodes.get(Number(opcode));
-    if (!op) { throw new Error(`uh oh: unknown opcode ${opcode} processing instruction ${s}!`) }
+    if (!op) { throw new Error(`uh oh: unknown opcode ${opcode}!`) }
 
     const modes = s.slice(0, -2).split('').reverse().map(n => Number(n));
 
-    // fill with zeros
-    if (modes.length < op.numArgs) {
-      for (const _ of range(op.numArgs - modes.length)) {
-        modes.push(0);
-      }
-    }
+    // resolve values as necessary
+    const offset = op.numArgs + 1;
+    const rawValues = this.data.slice(this.pointer + 1, this.pointer + offset);
 
-    return [op.compiled, modes];
+    const funcArgs = rawValues.map((raw, i) => {
+      return op.shouldResolveParam(i)
+             ? this._resolveValue(raw, modes[i] || MODES.POSITION)
+             : raw;
+    });
+
+    const pointerBefore = this.pointer;
+
+    op.run(this, funcArgs);
+
+    // do not bump pointer if instruction moved it.
+    if (this.pointer === pointerBefore) {
+      this.pointer += offset;
+    }
   }
 
   _resolveValue (val, mode) {
