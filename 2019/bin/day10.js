cb8eb7aa92e29ae647468b70ceaddc8dc4246a5d
Michael McClimon <michael@mcclimon.org>
2019-12-10 19:57:40 -0500

    day 10: a big refactor

--- 2019/bin/day10.js
+++ 2019/bin/day10.js
@@ -1,100 +1,118 @@
 const utils = require('../lib/advent-utils.js');
 
-const genMap = raw => {
-  const map = new Map();
+const AsteroidMap = class {
+  constructor (raw) {
+    this.map = new Map();
 
-  for (const [y, row] of raw.entries()) {
-    for (const [x, el] of row.split('').entries()) {
-      if (el === '#') {
-        const coords = [x, y];
-        map.set(coords.join(','), {
-          coords: coords,
-          dists: new Map(),
-        });
+    for (const [y, row] of raw.entries()) {
+      for (const [x, el] of row.split('').entries()) {
+        if (el === '#') {
+          const a = new Asteroid(x, y);
+          this.map.set(a.key, a);
+        }
       }
     }
   }
 
-  return map;
+  [Symbol.iterator] () {
+    return this.map.values();
+  }
 };
 
-const countAsteroids = (astMap, start) => {
-  // Calculate angle to every asteroid; we want the number of unique angles.
-  // While we're here, calculate the distance and store the distance from this
-  // to the other in the asteroid's state, which makes the other bit easier to
-  // compute later.
-  const seen = new Set();
-  for (const [k, asteroid] of astMap.entries()) {
-    if (asteroid === start) continue;
+const Asteroid = class {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+    this.key = x + ',' + y;
+    this.dists = new Map();
+    this.angles = new Map();
+  }
 
-    const [x, y] = asteroid.coords;
-    const adj = x - start.coords[0];
-    const opp = y - start.coords[1];
+  get coords () {
+    return [this.x, this.y];
+  }
 
-    const dist = Math.sqrt(Math.pow(opp, 2) + Math.pow(adj, 2)); // pythag
+  coordsRelativeTo (other) {
+    return [other.x - this.x, other.y - this.y];
+  }
 
-    let angle = Math.atan2(opp, adj) / (Math.PI / 180); // angle in degrees
+  distanceTo (other) {
+    const have = this.dists.get(other.key);
+    if (have) return have;
+
+    const [x, y] = this.coordsRelativeTo(other);
+    const dist = Math.sqrt((x * x) + (y * y)); // pythagoras
+
+    this.dists.set(other.key, dist);
+    return dist;
+  }
+
+  angleTo (other) {
+    const have = this.angles.get(other.key);
+    if (have) return have;
+
+    const [x, y] = this.coordsRelativeTo(other);
+    let angle = Math.atan2(y, x) / (Math.PI / 180); // angle in degrees
     angle = angle < 0 ? angle + 360 : angle; // map to 0-360
     angle = (angle + 90) % 360; // rotate so 0 is vertical
 
-    start.dists.set(k, { angle: angle, dist: dist, coords: asteroid.coords });
-    seen.add(angle);
+    this.angles.set(other.key, angle);
+    return angle;
   }
-
-  return seen.size;
 };
 
-const part1 = (raw) => {
-  const astMap = genMap(raw);
-  let max = 0;
-  let maxAst;
-
-  for (const asteroid of astMap.values()) {
-    const numSeen = countAsteroids(astMap, asteroid);
-    if (numSeen > max) {
-      max = numSeen;
-      maxAst = asteroid;
-    }
-  }
-
-  return [max, maxAst];
+// Calculate angle to every asteroid; we want the number of unique angles.
+const countAsteroids = (astMap, start) => {
+  return new Set(
+    Array.from(astMap).filter(a => a !== start).map(a => start.angleTo(a)),
+  ).size;
 };
 
-const part2 = (start) => {
-  // order by angle, then dist
-  const dists = Array.from(start.dists.values());
-  dists.sort((a, b) => {
-    if (a.angle < b.angle) return -1;
-    if (a.angle > b.angle) return 1;
+const part1 = (map) => {
+  return Array.from(map).reduce(
+    (acc, asteroid) => {
+      const num = countAsteroids(map, asteroid);
+      return num > acc[0] ? [num, asteroid] : acc;
+    },
+    [0, null],
+  );
+};
 
-    if (a.dist < b.dist) return -1;
-    if (a.dist > b.dist) return 1;
+const part2 = (map, start) => {
+  // order by angle, then dist; store as a map to make deletion fast
+  const sorted = new Map(Array.from(map).filter(a => a !== start).sort((a, b) => {
+    if (start.angleTo(a) < start.angleTo(b)) return -1;
+    if (start.angleTo(a) > start.angleTo(b)) return 1;
+
+    if (start.distanceTo(a) < start.distanceTo(b)) return -1;
+    if (start.distanceTo(a) > start.distanceTo(b)) return 1;
 
     return 0;
-  });
+  }).map(a => [a.key, a]));
 
   // run the thing
   const destroyed = [];
-  const s = new Set();
-  let lastAngle = null;
 
-  for (const asteroid of dists.values()) {
-    const coords = asteroid.coords;
+  while (sorted.size > 1) {
+    let lastAngle = null;
+    for (const [key, asteroid] of sorted.entries()) {
+      // skip asteroids that *just* became visible
+      const thisAngle = start.angleTo(asteroid);
+      if (thisAngle === lastAngle) continue;
 
-    // skip asteroids we've already hit and ones that just became visible
-    if (s.has(coords)) continue;
-    if (asteroid.angle === lastAngle) continue;
-
-    s.add(coords);
-    destroyed.push(coords);
-    lastAngle = asteroid.angle;
+      sorted.delete(key);
+      destroyed.push(asteroid);
+      lastAngle = thisAngle;
+    }
   }
 
   const target = destroyed[199];
-  return target[0] * 100 + target[1];
+  return target.x * 100 + target.y;
 };
 
 const raw = utils.fileLines('input/day10.txt');
-const [dist1, ast1] = part1(raw);
+const map = new AsteroidMap(raw);
+
+const [dist1, ast1] = part1(map);
 console.log(`part 1: ${dist1}`);
-console.log(`part 2: ${part2(ast1)}`);
+console.log(`part 2: ${part2(map, ast1)}`);
