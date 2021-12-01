6a4d14039612cf57b513f90859c93ae654ebd8f2
Michael McClimon <michael@mcclimon.org>
2021-12-01 08:43:19 -0500

    2021 day 1: refactor to reduce copypasta

--- 2021/bin/day1.ts
+++ 2021/bin/day1.ts
@@ -1,41 +1,15 @@
 import { fileLinesInt } from "../lib/advent-utils.ts";
 
+// const _test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 const lines = fileLinesInt("input/day1.txt");
 
-const _test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+const part1 = (nums: number[]): number =>
+  nums.reduce(([sum, prev], val) => {
+    return [val > prev ? sum + 1 : sum, val];
+  }, [0, Infinity])[0];
 
-function part1(nums: number[]): number {
-  let increases = 0;
-  let prev = Infinity;
-
-  for (const val of nums) {
-    if (val > prev) {
-      increases++;
-    }
-
-    prev = val;
-  }
-
-  return increases;
-}
-
-function part2(nums: number[]): number {
-  let increases = 0;
-  let prev = Infinity;
-
-  const len = nums.length;
-  for (let i = 0; i < len - 2; i++) {
-    const sum = nums[i] + nums[i + 1] + nums[i + 2];
-
-    if (sum > prev) {
-      increases++;
-    }
-
-    prev = sum;
-  }
-
-  return increases;
-}
+const part2 = (nums: number[]): number =>
+  part1(nums.map((el, idx) => el + nums[idx + 1] + nums[idx + 2]).slice(0, -2));
 
 console.log(part1(lines));
 console.log(part2(lines));
