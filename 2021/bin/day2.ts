116a3953c4b6f01dbefc71faa2906c0921b5e355
Michael McClimon <michael@mcclimon.org>
2021-12-02 09:06:47 -0500

    2021: hilariously overcomplicated solution for d2
    The metagame for JS is "do the problem in a single statement".

--- 2021/bin/day2.ts
+++ 2021/bin/day2.ts
@@ -1,6 +1,6 @@
 import { fileLines } from "../lib/advent-utils.ts";
 
-let _test = [
+const _test = [
   "forward 5",
   "down 5",
   "forward 8",
@@ -11,55 +11,21 @@ let _test = [
 
 const lines = fileLines("input/day2.txt");
 
-function part1(lines: string[]): number {
-  let h = 0;
-  let v = 0;
+const calculate = (lines: string[], part2: boolean): number =>
+  lines.map((line) => line.split(" "))
+    .map(([dir, num]): [string, number] => [dir, parseInt(num)])
+    .reduce(([h, v, aim], [dir, amt]) => [
+      dir === "forward" ? h + amt : h,
+      part2
+        ? (dir === "forward" ? v + aim * amt : v)
+        : (dir === "up" ? v - amt : dir === "down" ? v + amt : v),
+      dir === "up" ? aim - amt : dir === "down" ? aim + amt : aim,
+    ], [0, 0, 0])
+    .slice(0, 2)
+    .reduce((prod, cur) => prod * cur, 1);
 
-  for (const line of lines) {
-    const [dir, num] = line.split(" ");
-    const amt = parseInt(num);
-
-    switch (dir) {
-      case "forward":
-        h += amt;
-        break;
-      case "down":
-        v += amt;
-        break;
-      case "up":
-        v -= amt;
-        break;
-    }
-  }
-
-  return h * v;
-}
-
-function part2(lines: string[]): number {
-  let h = 0;
-  let v = 0;
-  let aim = 0;
-
-  for (const line of lines) {
-    const [dir, num] = line.split(" ");
-    const amt = parseInt(num);
-
-    switch (dir) {
-      case "forward":
-        h += amt;
-        v += aim * amt;
-        break;
-      case "down":
-        aim += amt;
-        break;
-      case "up":
-        aim -= amt;
-        break;
-    }
-  }
-
-  return h * v;
-}
+const part1 = (lines: string[]): number => calculate(lines, false);
+const part2 = (lines: string[]): number => calculate(lines, true);
 
 console.log(part1(lines));
 console.log(part2(lines));
