60aba43d649a7812ab116e933acd47c7258fe788
Michael McClimon <michael@mcclimon.org>
2021-12-08 08:42:26 -0500

    2021 day 7: a tiny tweak

--- 2021/bin/day7.ts
+++ 2021/bin/day7.ts
@@ -5,18 +5,13 @@ const _test = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
 const [line] = fileLines("input/day7.txt");
 const input = line.split(",").map((n) => parseInt(n));
 
-const cache = new Map();
+const cache: number[] = [];
 const costFor = (n: number, part2 = false): number => {
   if (!part2) return n;
-
-  const have = cache.get(n);
-  if (typeof have !== "undefined") return have;
-
   if (n <= 0) return 0;
 
-  const cost = n + costFor(n - 1, part2);
-  cache.set(n, cost);
-  return cost;
+  cache[n] ||= n + costFor(n - 1, part2);
+  return cache[n];
 };
 
 const calculate = (nums: number[], part2 = false): number => {
