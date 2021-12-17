8bc20e95397ffd64acfa4ca8e9f0a9c26c47c2b9
Michael McClimon <michael@mcclimon.org>
2021-12-17 08:51:53 -0500

    2021 day 17: rename a bunch of variables
    And write a kind of egregious for loop.

--- 2021/bin/day17.ts
+++ 2021/bin/day17.ts
@@ -2,56 +2,50 @@ const _test = [20, 30, -10, -5];
 const target = [201, 230, -99, -65];
 // target area: x=20..30, y=-10..-5
 
-const run = (xv: number, yv: number) => {
-  let xpos = 0, ypos = 0;
+const [xmin, xmax, ymin, ymax] = target;
 
+const run = (xv: number, yv: number) => {
   const ys = [];
 
-  while (true) {
-    xpos += xv;
-    ypos += yv;
+  for (
+    let xpos = 0, ypos = 0;
+    xpos <= xmax && ypos >= ymin;
+    xpos += xv, ypos += yv, xv += xv > 0 ? -1 : xv < 0 ? 1 : 0, yv--
+  ) {
     ys.push(ypos);
-    // console.log(xpos, ypos);
-
-    xv += xv > 0 ? -1 : xv < 0 ? 1 : 0;
-    yv -= 1;
 
     if (
-      target[0] <= xpos && xpos <= target[1] && target[2] <= ypos &&
-      ypos <= target[3]
+      xmin <= xpos && xpos <= xmax &&
+      ymin <= ypos && ypos <= ymax
     ) {
       // console.log("hit");
       return Math.max(...ys);
     }
-
-    if (xpos > target[1] || ypos < target[2]) return;
   }
 };
 
 const findBest = () => {
-  // find min/max x velocities
-  let maxY = 0;
-  let best = [0, 0];
-  for (let xv = 1; xv <= target[0]; xv++) {
-    for (let yv = 1; yv <= target[0]; yv++) {
+  let best = 0;
+
+  for (let xv = 1; xv <= xmin; xv++) {
+    for (let yv = 1; yv <= xmin; yv++) {
       const y = run(xv, yv);
-      if (y && y > maxY) {
-        best = [xv, yv];
-        maxY = y;
+      if (y && y > best) {
+        best = y;
       }
     }
   }
 
-  console.log(maxY);
+  console.log(best);
 };
 
 const findAll = () => {
   let total = 0;
-  for (let xv = 1; xv <= target[1]; xv++) {
-    for (let yv = target[2]; yv <= target[1]; yv++) {
+
+  for (let xv = 1; xv <= xmax; xv++) {
+    for (let yv = ymin; yv <= xmax; yv++) {
       const y = run(xv, yv);
       if (typeof y !== "undefined") {
-        // console.log(`${xv}, ${yv}`);
         total++;
       }
     }
