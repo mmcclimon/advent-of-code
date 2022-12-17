d974c4a3a8b0c4f2bef233b579b552b1b08ad794
Michael McClimon <michael@mcclimon.org>
2022-12-17 10:27:37 -0500

    2018 day 7: add a working part 1 solution
    I did this a few days ago, but I keep accidentally committing it with
    other things, so let's just commit it for real.

--- 2018/day7.py
+++ 2018/day7.py
@@ -1,8 +1,9 @@
 #!python
 
+from pprint import pp
 import copy
 import re
-from collections import defaultdict, deque
+from collections import defaultdict
 
 instructions = [
     'Step C must be finished before step A can begin.',
@@ -19,94 +20,65 @@ rev = defaultdict(set)
 
 for line in instructions:
     m = re.match(r'Step ([A-Z]).*before step ([A-Z])', line)
-    head = m.group(1)
-    tail = m.group(2)
-    deps[tail]  # get empty
-    deps[head].add(tail)
+    if m is not None:
+        head = m.group(1)
+        tail = m.group(2)
+        deps[head].add(tail)
+        deps[tail]  # get empty
+        rev[head]
+        rev[tail].add(head)
+
+# real one
+# with open('d7.txt') as f:
+#     for line in f.readlines():
+#         m = re.match(r'Step ([A-Z]).*before step ([A-Z])', line)
+#         if m is not None:
+#             head = m.group(1)
+#             tail = m.group(2)
+#             deps[head].add(tail)
+#             deps[tail]  # get empty
+#             rev[head]
+#             rev[tail].add(head)
 
 
-# Kahn's algorithm
-def kahn(deps):
-    L = []      # Empty list that will contain the sorted elements
-    S = set()   # Set of all nodes with no incoming edge
-    to_delete = set()
-    graph = copy.deepcopy(deps)
-    for k,v in graph.items():
-        if len(v) == 0:
-            S.add(k)
-
-    total = 0
+# this is kahn's algorithm, modified
+def part1(deps, rev):
+    deps = copy.deepcopy(deps)
+    rev = copy.deepcopy(rev)
+    S = {k for k in rev if len(rev[k]) == 0}
+    L = []
     while len(S):
-        n = S.pop()
-        del graph[n]
-
+        n = sorted(S)[0]
+        S.remove(n)
         L.append(n)
-        for m, edges in sorted(graph.items()):
-            if n in edges:
-                edges.remove(n)
-            if not edges:
+        for m in sorted(deps[n]):
+            deps[n].remove(m)
+            rev[m].remove(n)
+            if len(rev[m]) == 0:
                 S.add(m)
 
-        total += 1
-
-    return L
+    print(''.join(L))
 
 
-class Node:
-    def __init__(self, name, children):
-        self.name = name
-        self.children = children
-        self.t_mark = False
-        self.p_mark = False
+def part2(deps, rev):
+    N_WORKERS = 2
+    # starting elements
+    S = {k for k in rev if len(rev[k]) == 0}
+    finish_at = {}
+    tick = 0
 
-    def __repr__(self):
-        return "<Node {}, children={}".format(self.name, self.children)
-
-    def visit(self, L, nodelist):
-        print("visit({})".format(self.name))
-        if self.p_mark:
-            print("  already in L, nothing to do")
-            return
-        if self.t_mark:
-            raise Exception('not a DAG')
-        self.t_mark = True
-        for node_name in reversed(sorted(self.children)):
-            n = nodelist.d[node_name]
-            print("  at {}, gonna visit {}".format(self.name, node_name))
-            n.visit(L, nodelist)
-        self.p_mark = True
-        print("  adding {} to head".format(self.name))
-        L.appendleft(self)
-
-class NodeList:
-    def __init__(self, d):
-        self.d = d
-
-    def add(self, node):
-        self.d[ node.name ] = node
-
-    def has_unmarked_nodes(self):
-        for node in self.d.values():
-            if not node.t_mark and not node.p_mark:
-                return True
-        return False
+    while len(S):
+        todo = sorted(S)
 
 
+    # at every tick:
+    # - are there things available
+    #   if yes, and there is a worker available:
+    #     - start work, keep track of when to finish
+    # - when a thing finishes:
+    #   - do bookkeeping for edges
+    #   - add free things to avaiable work
 
-nl = NodeList({})
-for k, v in deps.items():
-    print(k, v)
-    nl.add(Node(k,v))
 
-print(deps)
-
-L = deque()
-while nl.has_unmarked_nodes():
-    to_visit = None;
-    for node in nl.d.values():
-        node.visit(L, nl)
-
-output = []
-for n in L:
-    output.append(n.name)
-print(output)
+part1(deps, rev)
+part2(deps, rev)
