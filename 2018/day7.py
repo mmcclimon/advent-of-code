22d483eea65d5d383460b60bfbfabd4559bf1210
Michael McClimon <michael@mcclimon.org>
2022-12-18 19:09:44 -0500

    2018 day 7: finish part 2
    This is not interesting, really, just some fiddly accounting work.

--- 2018/day7.py
+++ 2018/day7.py
@@ -1,44 +1,8 @@
 #!python
 
-from pprint import pp
 import copy
 import re
 from collections import defaultdict
-
-instructions = [
-    'Step C must be finished before step A can begin.',
-    'Step C must be finished before step F can begin.',
-    'Step A must be finished before step B can begin.',
-    'Step A must be finished before step D can begin.',
-    'Step B must be finished before step E can begin.',
-    'Step D must be finished before step E can begin.',
-    'Step F must be finished before step E can begin.',
-]
-
-deps = defaultdict(set)
-rev = defaultdict(set)
-
-for line in instructions:
-    m = re.match(r'Step ([A-Z]).*before step ([A-Z])', line)
-    if m is not None:
-        head = m.group(1)
-        tail = m.group(2)
-        deps[head].add(tail)
-        deps[tail]  # get empty
-        rev[head]
-        rev[tail].add(head)
-
-# real one
-# with open('d7.txt') as f:
-#     for line in f.readlines():
-#         m = re.match(r'Step ([A-Z]).*before step ([A-Z])', line)
-#         if m is not None:
-#             head = m.group(1)
-#             tail = m.group(2)
-#             deps[head].add(tail)
-#             deps[tail]  # get empty
-#             rev[head]
-#             rev[tail].add(head)
 
 
 # this is kahn's algorithm, modified
@@ -57,28 +21,60 @@ def part1(deps, rev):
             if len(rev[m]) == 0:
                 S.add(m)
 
-    print(''.join(L))
+    print("part 1: " + ''.join(L))
 
 
 def part2(deps, rev):
-    N_WORKERS = 2
-    # starting elements
-    S = {k for k in rev if len(rev[k]) == 0}
+    N_WORKERS = 5
+    S = {k for k in rev if len(rev[k]) == 0}    # starting elements
     finish_at = {}
     tick = 0
 
-    while len(S):
-        todo = sorted(S)
+    while True:
+        # Do bookkeeping for anything finished and add free things to
+        # available work
+        to_remove = [n for n in finish_at.keys() if finish_at[n] == tick]
+        for n in to_remove:
+            finish_at.pop(n)
+            for m in sorted(deps[n]):
+                deps[n].remove(m)
+                rev[m].remove(n)
+                if len(rev[m]) == 0:
+                    S.add(m)
+
+        # if there are things to do, and workers available, start them
+        if len(S):
+            todo = list(reversed(sorted(S)))
+            workers_avail = N_WORKERS - len(finish_at)
+
+            for i in range(min(workers_avail, len(todo))):
+                item = todo.pop()
+                S.remove(item)
+                time_to_finish = (ord(item) - ord('A')) + 61
+                finish_at[item] = tick + time_to_finish
+
+        if len(finish_at) == 0 and len(S) == 0:
+            break
+
+        tick += 1
+
+    print(f"part 2: {tick}")
 
 
-    # at every tick:
-    # - are there things available
-    #   if yes, and there is a worker available:
-    #     - start work, keep track of when to finish
-    # - when a thing finishes:
-    #   - do bookkeeping for edges
-    #   - add free things to avaiable work
+if __name__ == '__main__':
+    deps = defaultdict(set)
+    rev = defaultdict(set)
 
+    with open('d7.txt') as f:
+        for line in f.readlines():
+            m = re.match(r'Step ([A-Z]).*before step ([A-Z])', line)
+            if m is not None:
+                head = m.group(1)
+                tail = m.group(2)
+                deps[head].add(tail)
+                deps[tail]  # get empty
+                rev[head]
+                rev[tail].add(head)
 
-part1(deps, rev)
-part2(deps, rev)
+    part1(deps, rev)
+    part2(deps, rev)
